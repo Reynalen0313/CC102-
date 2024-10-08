05de1e5ef1dbb3ddec88354b08d0206eafa20a6b
Reynalen0313 <obligacionreynalen3@gmail.com>
2024-10-08 09:42:40 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Our Lady of Lourdes College Foundation"<< endl;
+	cout<<"College of Computer Studies"<< endl;
+	cout<<"Bachelor of Science in Information Technology"<< endl;
+	cout<<"Reynalen M. Obligacion"<< endl;
+	cout<<"Panganiban Camarines Norte"<< endl;
+	return 0;
+	
+}
---
+++ 04Basicinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Reynalen"<< endl;
+	cout<<"BSIT-1"<< endl;
+	cout<<"OLLCF-DAET,CAMARINES NORTE"<< endl;
+	return 0;
+	
+}
