f6aa11a421f02e1b25c68beb73773ca2c38c8154
Reynalen0313 <obligacionreynalen3@gmail.com>
2024-11-13 09:19:36 +0800

    Add files via upload

---
+++ Temperature.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace;
+
+int main()
+
+{
+	int temperature;
+	
+	cout << "Enter the temperature in degree:"; 
+	cin >> temperature;
+	
+	if(Temperature < 32) {
+		cout << "Bring a heavy jacket";
+	    
+    }
+	else if(Temperature && 32 and 50) {
+		cout << "Bring a light jacket";
+	    
+    }
+    else if(Temperature > 50) {
+	    cout << " Not to bring any jacket";
+	    
+    }
+	return 0;
+		
+}		
