d720d78844a5be2f1c28b3210a358789a4d54838
Reynalen0313 <obligacionreynalen3@gmail.com>
2024-11-21 09:43:02 +0800

    Update and rename Temperature.cpp to ACT.01Temperature.cpp

--- ACT.01Temperature.cpp
+++ ACT.01Temperature.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 
 {
-	int temperature;
-	cout << "Enter the temperature in degree:"; 
-	cin >> temperature;
-	if(temperature < 32) {
-		cout << "Bring a heavy jacket";
+    int temperature;
+    cout << "Enter the temperature in degree:"; 
+    cin >> temperature;
+    if(temperature < 32) {
+	cout << "Bring a heavy jacket";
 	    
     } else if(temperature >= 32 && temperature <= 50) {
 		cout << "Bring a light jacket";
@@ -17,6 +17,5 @@ int main()
     } else if(temperature > 50) {
 	    cout << "Not to bring any jacket";
     }
-	return 0;
-		
-}		
+    return 0;		
+}
